4246a29459e0076d7d34fc99cceeef84d2257ecb
muhammadsaripuddin <aripedward339@gmail.com>
2017-10-25 07:23:53 +0700

    perbaikan

--- tugas praktek 0201.cpp
+++ tugas praktek 0201.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main(){
-	nilai=nilai+1;
-	int nilai;
-	int 1Nilai;
 	
+	int nilai;
+	int Nilai1;
+	nilai=nilai+1;
 	cout<< nilai;
-	cout<< 1Nilai;
+	cout<< Nilai1;
 	
 	
 }
