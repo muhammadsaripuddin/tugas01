84bcf47ef813ea29ffd2302725d6a3b5cc8d464e
muhammadsaripuddin <aripedward339@gmail.com>
2017-10-25 08:03:52 +0700

    perbaikan jumlah box parket

--- praktek 0204.cpp
+++ praktek 0204.cpp
@@ -9,7 +9,7 @@ int main(){
 	double LP=1.87596;
 	cout<<"Luas 1 box Parket	:"<<LP<<"m2"<<endl;
 	double JP=LR/LP;
-	cout<<"jumlah box parket	:"<<JP<endl;
+	cout<<"jumlah box parket	:"<<JP<<endl;
 	double BPR=JP*500;
 	cout<<"biaya parket (Rp)	:"<<BPR<<endl;
 	int BPS=LR*20;
